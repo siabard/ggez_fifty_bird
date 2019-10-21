d8c0f439d86866645a811ebb6814736d1bd79a8f
Yeonho Jang <siabard@gmail.com>
2019-10-21 21:56:25 +0900

    Parallax 테스트

--- src/bin/main.rs
+++ src/bin/main.rs
@@ -1,15 +1,16 @@
 use ggez::event::{self, EventHandler};
 use ggez::graphics;
 use ggez::input::keyboard::{KeyCode, KeyMods};
+use ggez::timer;
 use ggez::{Context, ContextBuilder, GameResult};
 
 use ggez::nalgebra as na;
 
-const WINDOW_WIDTH: f32 = 1280.;
-const WINDOW_HEIGHT: f32 = 720.;
-
 const VIRTUAL_WIDTH: u16 = 512;
 const VIRTUAL_HEIGHT: u16 = 288;
+
+const WINDOW_WIDTH: f32 = 1024.;
+const WINDOW_HEIGHT: f32 = 576.;
 
 const X_RATIO: f32 = WINDOW_WIDTH / (VIRTUAL_WIDTH as f32);
 const Y_RATIO: f32 = WINDOW_HEIGHT / (VIRTUAL_HEIGHT as f32);
@@ -33,6 +34,8 @@ struct MyGame {
     message: ggez::graphics::Text,
     background: ggez::graphics::Image,
     ground: ggez::graphics::Image,
+    background_pos_x: f32,
+    ground_pos_x: f32,
 }
 
 impl MyGame {
@@ -56,12 +59,14 @@ impl MyGame {
             message,
             background,
             ground,
+            background_pos_x: 0.,
+            ground_pos_x: 0.,
         })
     }
 }
 
 impl MyGame {
-    fn draw_canvas(&mut self, ctx: &mut Context) -> GameResult {
+    fn draw_canvas(&mut self, ctx: &mut Context, dt: f64) -> GameResult {
         graphics::set_canvas(ctx, Some(&self.buffer));
 
         graphics::clear(ctx, graphics::Color::from_rgba(30, 30, 0, 255));
@@ -70,29 +75,27 @@ impl MyGame {
         let dest_point = na::Point2::new((VIRTUAL_WIDTH as f32 - span) / 2.0, 20.0);
         graphics::draw(ctx, &self.message, (dest_point, 0.0, graphics::WHITE))?;
 
+        self.background_pos_x = (self.background_pos_x + 200. * (dt as f32)) % 413.;
+        self.ground_pos_x = (self.ground_pos_x + 120. * (dt as f32)) % (VIRTUAL_WIDTH as f32);
         graphics::draw(
             ctx,
             &self.background,
-            (na::Point2::new(0., 0.), 0.0, graphics::WHITE),
+            (
+                na::Point2::new(-self.background_pos_x, 0.),
+                0.0,
+                graphics::WHITE,
+            ),
         )?;
 
         graphics::draw(
             ctx,
             &self.ground,
             (
-                na::Point2::new(0., VIRTUAL_HEIGHT as f32 - 16.),
+                na::Point2::new(-self.ground_pos_x, VIRTUAL_HEIGHT as f32 - 16.),
                 0.0,
                 graphics::WHITE,
             ),
         )?;
-
-        let rect = graphics::Mesh::new_rectangle(
-            ctx,
-            graphics::DrawMode::fill(),
-            graphics::Rect::new(0., 0., VIRTUAL_WIDTH as f32, VIRTUAL_HEIGHT as f32),
-            graphics::WHITE,
-        )?;
-        graphics::draw(ctx, &rect, graphics::DrawParam::default())?;
 
         graphics::set_canvas(ctx, None);
         Ok(())
@@ -100,17 +103,21 @@ impl MyGame {
 }
 
 impl EventHandler for MyGame {
-    fn update(&mut self, _ctx: &mut Context) -> GameResult {
+    fn update(&mut self, ctx: &mut Context) -> GameResult {
+        // dt(delta) 얻어오기
+        let dt = timer::duration_to_f64(timer::delta(ctx));
+
+        // 그려야할 스크롤의 위치를 계산하기
+        self.draw_canvas(ctx, dt);
         Ok(())
     }
 
     fn draw(&mut self, ctx: &mut Context) -> GameResult {
-        self.draw_canvas(ctx);
         graphics::clear(ctx, graphics::WHITE);
         let dims = self.buffer.image().dimensions();
 
         let dest_point = na::Point2::new(0., 0.);
-        let scale = na::Vector2::new(X_RATIO, Y_RATIO);
+        let scale = na::Vector2::new(X_RATIO * X_RATIO, Y_RATIO * Y_RATIO);
 
         graphics::draw(
             ctx,
